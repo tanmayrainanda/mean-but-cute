289c395e2cb1ab286a3e4910bb6b00bd1f50278f
shivangiag08 <shivangiag08@gmail.com>
2023-10-30 20:46:08 +0530

    updated file

--- covariance_test.py
+++ covariance_test.py
@@ -4,7 +4,7 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 
 # Alpha Vantage API key
-api_key = '0QCKUKJ70VNZ6MKO'
+api_key = 'IKLN84X4ZS01P4YI'
 
 # Function to get stock data
 def get_stock_data(symbol, api_key):
@@ -12,6 +12,7 @@ def get_stock_data(symbol, api_key):
     data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
     return data['4. close']
 
+df = pd.DataFrame()
 # List of stocks in portfolio with number of shares
 stocks = {'AAPL': 100, 'MSFT': 100, 'GOOGL': 100}
 
@@ -28,16 +29,29 @@ print ("Buy Value", buy_value)
 #current price and value of portfolio
 current_price = {}
 current_value = {}
+
+#add closing price of stocks for the last 2 months to dataframe
 for stock in stocks:
-    current_price[stock] = get_stock_data(stock, api_key)[0]
+    df[stock] = get_stock_data(stock, api_key)
+    current_price[stock] = df[stock][0]
     current_value[stock] = current_price[stock] * stocks[stock]
+
+# convert 'date' to datetime format
+df.index = pd.to_datetime(df.index)
+# filter df to only have data post 2021
+df = df.loc['2021':]
 
 #print ("Current Price of each share", current_price)
 print ("Current Value of Portfolio", current_value)
 
-#mean return value of each stock for the period of the last 2 months
+""" #mean return value of each stock for the period of the last 2 months
 mean_return = {}
 for stock in stocks:
-    mean_return[stock] = get_stock_data(stock, api_key).pct_change().mean()
+    mean_return[stock] = get_stock_data(stock, api_key).mean()
 
-print ("Mean Return", mean_return)
+print ("Mean Return", mean_return) """
+
+#filter the df to have closing prices only from 2021 onwards
+
+
+#calculate mean return of each stock for the period of the last 2 months
