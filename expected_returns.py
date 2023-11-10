df550c2f5e50ec347ab3c121860de477b35ab080
Tanmay Rai Nanda <Stormrider7600@gmail.com>
2023-11-10 16:01:45 +0530

    added weights

--- expected_returns.py
+++ expected_returns.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pypfopt import expected_returns
 from pypfopt.efficient_frontier import EfficientFrontier
-
+from pypfopt import risk_models
+from pypfopt.discrete_allocation import DiscreteAllocation
 
 # Function to get stock data
 def get_stock_data(symbol):
@@ -13,69 +14,50 @@ def get_stock_data(symbol):
     return data['Close']
 
 df = pd.DataFrame()
-# List of stocks in portfolio with number of shares
+
 stocks = {'UNH': 1, 'TSLA': 1, 'GOOGL': 1}
-
-#original buy price
 buy_price = {'UNH': 100, 'TSLA': 200, 'GOOGL': 100}
+buy_value = {stock: buy_price[stock] * stocks[stock] for stock in stocks}
 
-#value of portfolio at time of purchase
-buy_value = {}
-for stock in stocks:
-    buy_value[stock] = buy_price[stock] * stocks[stock]
+print("Buy Value", buy_value)
 
-print ("Buy Value", buy_value)
-
-#current price and value of portfolio
 current_price = {}
 current_value = {}
 
-#add closing price of stocks for the last 2 months to dataframe
 for stock in stocks:
     df[stock] = get_stock_data(stock)
     current_price[stock] = df[stock][0]
     current_value[stock] = current_price[stock] * stocks[stock]
+print("Current Value of Portfolio", current_value)
 
-#print ("Current Price of each share", current_price)
-print ("Current Value of Portfolio", current_value)
+# mean return value of each stock for the period of the last 2 months
+mean_return = {stock: get_stock_data(stock).mean() for stock in stocks}
+print("Mean Return", mean_return)
 
-#mean return value of each stock for the period of the last 2 months
-mean_return = {}
+# Calculate the value of return that each stock has given in 2-month intervals from Jan 1st, 2021, to the current date
 for stock in stocks:
-    mean_return[stock] = get_stock_data(stock).mean()
+    df[stock + 'Return'] = df[stock] - buy_price[stock]
 
-print ("Mean Return", mean_return)
-
-#Calculate the value of return that each stock has given in 2 month intervals from jan 1st 2021 to current date
-for stock in stocks:
-    df[stock + 'Return'] = df[stock]-buy_price[stock]
-
-print(df)
-
-#Calculate the covariance of the returns for this portfolio
-for i in stocks:
-    for j in stocks:
-        if i != j:
-            cov = df[i + 'Return'].cov(df[j + 'Return'])
-            print("Covariance between", i, "and", j, "is", cov)
-
-#display the covariance matrix as a heatmap
-sns.heatmap(df.cov())
+cov_matrix = df.pct_change().cov()
+sns.heatmap(cov_matrix)
 plt.show()
 
-#Calculate the expected return of the portfolio
-expected_return = 0
-for stock in stocks:
-    expected_return += mean_return[stock] * stocks[stock]
-
+# Calculate the expected return of the portfolio
+expected_return = sum(mean_return[stock] * stocks[stock] for stock in stocks)
 print("Expected Return of Portfolio", expected_return)
-
-#Calculate the expected return of the portfolio using pypfopt
 mu = expected_returns.capm_return(df)
-mu.plot.barh(figsize=(10,6))
+mu.plot.barh(figsize=(10, 6))
 
-ef = EfficientFrontier(mu, df.cov())
+# calculate efficient frontier
+ef = EfficientFrontier(mu, cov_matrix, weight_bounds=(0, 1))
 weights = ef.max_sharpe()
-
+print(weights)
 cleaned_weights = ef.clean_weights()
-print(cleaned_weights)
+print(dict(cleaned_weights))
+
+ef.portfolio_performance(verbose=True)
+
+# Convert current_value to a pandas Series and drop any NaN values
+current_value_series = pd.Series(current_value).dropna()
+
+da = DiscreteAllocation(weights, current_value_series, total_portfolio_value=10000)
