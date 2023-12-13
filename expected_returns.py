e04622b52334d2f50e8a3910bbd27f27f3887a48
Tanmay Rai Nanda <Stormrider7600@gmail.com>
2023-12-13 18:29:27 +0530

    updated ticker list

--- expected_returns.py
+++ expected_returns.py
@@ -10,8 +10,8 @@ from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 import datetime
 from pypfopt import plotting
 
-start_date = datetime.datetime(2021,4,1)
-end_date = datetime.datetime(2023,12,13)
+start_date = datetime.datetime(2021, 4, 1)
+end_date = datetime.datetime(2023, 12, 13)
 
 # Function to get stock data
 def get_stock_data(symbol):
@@ -19,51 +19,52 @@ def get_stock_data(symbol):
     data = data['Adj Close']
     return data
 
-ticker_list = ['UNH', 'TSLA', 'GOOGL','IOC.NS']
+ticker_list = [ 'AMZN', 'JPM', 'JNJ','PG','CVX','WMT','NVDA','MMM','UNH','TSLA','HD','KO']
 portfolio = get_stock_data(ticker_list)
-portfolio.to_csv("portfolio.csv",index=True)
-portfolio = pd.read_csv("portfolio.csv",parse_dates=True,index_col="Date")
+portfolio.to_csv("portfolio.csv", index=True)
+portfolio = pd.read_csv("portfolio.csv", parse_dates=True, index_col="Date")
 
 df = pd.DataFrame()
 
-stocks = {'UNH': 1, 'TSLA': 1, 'GOOGL': 1, 'IOC.NS': 1}
-buy_price = {'UNH': 100, 'TSLA': 200, 'GOOGL': 100, 'IOC.NS': 100}
-buy_value = {stock: buy_price[stock] * stocks[stock] for stock in stocks}
+# Initial portfolio composition
+initial_weights = {'AMZN': 0.08, 'JPM': 0.08, 'JNJ': 0.08, 'PG': 0.08, 'CVX': 0.08, 'WMT': 0.08, 'NVDA': 0.08, 'MMM': 0.08, 'UNH': 0.08, 'TSLA': 0.08, 'HD': 0.08, 'KO': 0.08}
+budget = 30000  # Specify your budget
 
-print("Buy Value", buy_value)
+# Calculate the initial number of shares based on the initial weights and budget
+initial_allocation = {stock: round(initial_weights[stock] * budget / get_stock_data(stock)[0], 0) for stock in initial_weights}
 
-current_price = {}
-current_value = {}
+print("Initial Allocation:", initial_allocation)
 
-for stock in stocks:
+for stock in initial_allocation:
     df[stock] = get_stock_data(stock)
-    current_price[stock] = df[stock][0]
-    current_value[stock] = current_price[stock] * stocks[stock]
-print("Current Value of Portfolio", current_value)
 
-# mean return value of each stock for the period of the last 2 months
-mean_return = {stock: get_stock_data(stock).mean() for stock in stocks}
-print("Mean Return", mean_return)
+# Calculate current portfolio value
+current_value = {stock: df[stock][0] * initial_allocation[stock] for stock in initial_allocation}
+print("Current Value of Portfolio:", current_value)
+
+# Calculate mean return value of each stock for the period of the last 2 months
+mean_return = {stock: get_stock_data(stock).mean() for stock in initial_allocation}
+print("Mean Return:", mean_return)
 
 # Calculate the value of return that each stock has given in 2-month intervals from Jan 1st, 2021, to the current date
-for stock in stocks:
-    df[stock + 'Return'] = df[stock] - buy_price[stock]
+for stock in initial_allocation:
+    df[stock + 'Return'] = df[stock] - df[stock][0]
 
-cov_matrix = df.pct_change().cov()
-sns.heatmap(cov_matrix)
-plt.show()
+# cov_matrix = df.pct_change().cov()
+# sns.heatmap(cov_matrix)
+# plt.show()
 
 sample_cov = risk_models.sample_cov(portfolio, frequency=252)
 S = risk_models.CovarianceShrinkage(portfolio).ledoit_wolf()
 plotting.plot_covariance(S);
 
 # Calculate the expected return of the portfolio
-expected_return = sum(mean_return[stock] * stocks[stock] for stock in stocks)
-print("Expected Return of Portfolio", expected_return)
+expected_return = sum(mean_return[stock] * initial_allocation[stock] for stock in initial_allocation)
+print("Expected Return of Portfolio:", expected_return)
 mu = expected_returns.capm_return(portfolio)
 mu.plot.barh(figsize=(10, 6))
 
-# calculate efficient frontier
+# Calculate efficient frontier
 ef = EfficientFrontier(mu, S)
 weights = ef.max_sharpe()
 
@@ -72,10 +73,17 @@ print(dict(cleaned_weights))
 
 ef.portfolio_performance(verbose=True)
 
-# Convert current_value to a pandas Series and drop any NaN values
-current_value_series = pd.Series(current_value).dropna()
+# Get the latest prices for each stock
+latest_prices = get_latest_prices(portfolio)
 
-da = DiscreteAllocation(weights, current_value_series, total_portfolio_value=10000)
+# Convert the current allocation to units of stocks based on the latest prices
+current_allocation_units = {stock: current_value[stock] / latest_prices[stock] for stock in current_value}
+
+# Convert current_allocation_units to a pandas Series and drop any NaN values
+current_allocation_series = pd.Series(current_allocation_units).dropna()
+
+# Calculate the discrete allocation based on the latest prices and total_portfolio_value
+da = DiscreteAllocation(weights, current_allocation_series, total_portfolio_value=budget)
 
 allocation, leftover = da.greedy_portfolio()
 print("Discrete allocation:", allocation)
@@ -93,7 +101,7 @@ print("Sample portfolio volatilities:", stds)
 # Plot efficient frontier with Monte Carlo sim
 ef = EfficientFrontier(mu, S)
 
-fig, ax = plt.subplots(figsize= (10,10))
+fig, ax = plt.subplots(figsize=(10, 10))
 plotting.plot_efficient_frontier(ef, ax=ax, show_assets=False)
 
 # Find and plot the tangency portfolio
@@ -103,7 +111,7 @@ ret_tangent, std_tangent, _ = ef2.portfolio_performance()
 
 # Plot random portfolios
 ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r")
-ax.scatter(std_tangent, ret_tangent, c='red', marker='X',s=150, label= 'Max Sharpe')
+ax.scatter(std_tangent, ret_tangent, c='red', marker='X', s=150, label='Max Sharpe')
 
 im = ax.scatter(stds, rets, c=sharpes, marker=".", cmap="viridis_r")
 
